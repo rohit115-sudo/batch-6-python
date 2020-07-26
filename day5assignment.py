520162ae1774f8c2219c8aa4ad3578eee32284fe
rohit115-sudo <68591721+rohit115-sudo@users.noreply.github.com>
2020-07-26 18:03:03 +0530

    Add files via upload

---
+++ day5assignment.py
@@ -0,0 +1,43 @@
+#  list
+numbers=[0,1,2,10,4,1,0,56,20,1,30,56,0,4]
+numbers.sort()
+
+
+print(numbers)
+
+numbers = [0,1,2,10,4,1,0,56,20,1,30,56,0,4]
+
+# Sorting list of Integers in descending
+numbers.sort(reverse=True)
+
+print(numbers)
+
+arr =  [0,1,2,10,4,1,0,56,20,1,30,56,0,4]
+n = len(arr)
+
+print("Original array:", end=" ")
+(arr, n)
+
+(arr, n)
+
+print("\nModified array: ", end=" ")
+print(arr, n)
+
+
+# Q2
+list1 = [10,20,40,60,70,80]
+list2 = [5,15,25,35,45,60]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+
+
+
+
+
+
+
+
